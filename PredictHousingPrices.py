50103dada57a369c8aa832f94c1c39e4c0483091
qatshana <qatshan@gmail.com>
2018-06-26 19:22:12 -0400

    refactored code

--- PredictHousingPrices.py
+++ PredictHousingPrices.py
@@ -17,60 +17,73 @@ from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 
-USAhousing = pd.read_csv('USA_Housing.csv')
-# get basic information about the dataset
-USAhousing.head()
-USAhousing.describe()
-USAhousing.info()
+def load_training_data():
+	'''
+    Explore data - relationship between different variables
 
-# get dataset columns
-USAhousing.columns
+    '''
+	USAhousing = pd.read_csv('USA_Housing.csv')
+	return USAhousing
 
-# explore data - relationship between different variables
-sns.pairplot(USAhousing)
+def explore_data(USAhousing):
+	'''
+    Explore data - relationship between different variables
 
-# get standard distribution for target (Price)
+    '''
+	USAhousing.head()
+	USAhousing.describe()
+	USAhousing.info()
 
-sns.distplot(USAhousing['Price'])  # price is normally distributed 
+	# get dataset columns
+	USAhousing.columns
 
-sns.heatmap(USAhousing.corr())  # get headmap to get correlation with different variables
+	# explore data - relationship between different variables
+	sns.pairplot(USAhousing)
 
-#Training a Linear Regression Mode
-
-# define inputs (ignore address)
-X = USAhousing[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
-               'Avg. Area Number of Bedrooms', 'Area Population']]
-
-#define output file
-y = USAhousing['Price']
-
-#Create train and est sets using Train Test Split, 40% test data and 60% training data
+	# get standard distribution for target (Price)
+	sns.distplot(USAhousing['Price'])  # price is normally distributed 
+	sns.heatmap(USAhousing.corr())  # get headmap to get correlation with different variables
 
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+def process_data(train):
+	'''
+    Process data, nothing to process
+    '''
+	return train
 
 
-# Train Model
+def split_feature_target(USAhousing):
+	X = USAhousing[['Avg. Area Income', 'Avg. Area House Age', 'Avg. Area Number of Rooms',
+	               'Avg. Area Number of Bedrooms', 'Area Population']] # define inputs (ignore address)
+	y = USAhousing['Price'] #define output file
+	return X,y 
 
-lm = LinearRegression()
-lm.fit(X_train,y_train)
+if __name__=='__main__':
 
-# Model Evaluation
-print(lm.intercept_)
-coeff_df = pd.DataFrame(lm.coef_,X.columns,columns=['Coefficient']) # use pandas data frame to organize and display coeff
+    train=load_training_data() # load data
+    train=process_data(train)  # process/clean data
 
-print(coeff_df)
+    X,y=split_feature_target(train)
 
-# Perform predictions
+    #Create train and est sets using Train Test Split, 40% test data and 60% training data
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
 
-predictions = lm.predict(X_test)
+    lm = LinearRegression()
+    lm.fit(X_train,y_train)
+    # Model Evaluation
+    print("Intercept point = %3.2f "%lm.intercept_)
+    coeff_df = pd.DataFrame(lm.coef_,X.columns,columns=['Coefficient']) # use pandas data frame to organize and display coeff
+    print(coeff_df)
 
-# Model Evaluation - visual
-plt.scatter(y_test,predictions)
-sns.distplot((y_test-predictions),bins=50);
-
-# Model Evaluation - Metrics MSE-RMSE-MAE
-
-print('MAE:', metrics.mean_absolute_error(y_test, predictions))
-print('MSE:', metrics.mean_squared_error(y_test, predictions))
-print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
+    # Perform predictions
+    predictions = lm.predict(X_test)
+	
+	# Model Evaluation - visual
+    plt.scatter(y_test,predictions)
+    sns.distplot((y_test-predictions),bins=50);
+    
+    # Model Evaluation - Error
+    print("\n\nError Calculations")
+    print('MAE:', metrics.mean_absolute_error(y_test, predictions))
+    print('MSE:', metrics.mean_squared_error(y_test, predictions))
+    print('RMSE:', np.sqrt(metrics.mean_squared_error(y_test, predictions)))
